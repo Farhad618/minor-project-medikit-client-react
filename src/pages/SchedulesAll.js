ddde025e814cd384a5348a9e0599997b4f3c753c
Farhad618 <farhadmolla618@gmail.com>
2023-11-17 15:36:00 +0530

    update data ui setup

--- src/pages/SchedulesAll.js
+++ src/pages/SchedulesAll.js
@@ -6,6 +6,11 @@ function ScheduleAll() {
   const [data, setData] = useState([]);
   const [status, setStatus] = useState(undefined);
   const [result, setResult] = useState(null);
+  const [uptadeId, setUpdateId] = useState(null);
+  const [formInputData, setformInputData] = useState({
+    s_time: '',
+    s_activation: ''
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,8 +31,10 @@ function ScheduleAll() {
       }
     };
 
+    console.log("updateId", uptadeId)
+
     fetchData();
-  }, [result, status]);
+  }, [result, status, uptadeId]);
 
   const handleDelete = async (id) => {
     // console.log(id, "delete")
@@ -49,6 +56,48 @@ function ScheduleAll() {
       console.error('Error fetching data:', error);
     }
   }
+  const formatTime = (timeString) => {
+    const [hours, minutes] = timeString.split(':');
+    const hoursString = hours.padStart(2, '0');
+    const minutesString = minutes.padStart(2, '0');
+    return `${hoursString}:${minutesString}`;
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // console.log('Form submitted with data:', this.state);
+    try {
+      // console.log(123123)
+      const response = await fetch(`http://localhost:3001/api/data/schedule-update/${uptadeId}`, {
+        method: "PUT", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          p_email: localStorage.getItem("medikit-email"),
+          s_time: formInputData.s_time,
+          s_activation: formInputData.s_activation,
+        }),
+      });
+
+      const json = await response.json();
+      setResult(json);
+      setStatus(json.status)
+      setUpdateId(null)
+      setformInputData({
+        s_time: null,
+        s_activation: null
+      })
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  const handleInputChange = (event) => {
+    const newInput = (data) => ({ ...data, [event.target.name]: event.target.value })
+    setformInputData(newInput)
+  }
+
+
 
   return (
     <>
@@ -76,12 +125,50 @@ function ScheduleAll() {
         <tbody>
           {data.length === 0 ? <tr><td>Loading...</td></tr> : (
             data.map((time) => (
-              <tr key={time._id}>
-                <td>{time.s_time}</td>
-                <td>{time.s_activation}</td>
-                <td><button className="btn" onClick={() => handleDelete(time._id)}><i className="bi bi-trash3-fill text-danger"></i></button></td>
-                <td>Update</td>
-              </tr>
+              time._id === uptadeId ? (
+                <tr key={time._id}>
+                  {/* <form onSubmit={(e) => handleSubmit(e)}> */}
+                  <td>
+                    <input
+                      type="time"
+                      className="form-control"
+                      id="Time"
+                      name="s_time"
+                      // defaultValue={formatTime(time.s_time)}
+                      value={formatTime(formInputData.s_time)}
+                      autoComplete="on"
+                      onChange={(e) => handleInputChange(e)}
+                    />
+                  </td>
+                  <td>
+                    <input type="text"
+                      className="form-control"
+                      id="Activation"
+                      name="s_activation"
+                      // defaultValue={time.s_activation}
+                      value={formInputData.s_activation}
+                      autoComplete="on"
+                      onChange={(e) => handleInputChange(e)}
+                    />
+                  </td>
+                  <td><button className="btn" onClick={() => handleDelete(time._id)}><i className="bi bi-trash3-fill text-danger"></i></button></td>
+                  <td><button className="btn" onClick={handleSubmit}><i className="bi bi-pen text-info"></i></button></td>
+                  {/* </form> */}
+                </tr>
+              ) : (
+                <tr key={time._id}>
+                  <td>{time.s_time}</td>
+                  <td>{time.s_activation}</td>
+                  <td><button className="btn" onClick={() => handleDelete(time._id)}><i className="bi bi-trash3-fill text-danger"></i></button></td>
+                  <td><button className="btn" onClick={() => {
+                    setUpdateId(time._id); 
+                    setformInputData({
+                      s_time: time.s_time,
+                      s_activation: time.s_activation
+                    });
+                  }}><i className="bi bi-pen-fill text-info"></i></button></td>
+                </tr>
+              )
             ))
           )}
         </tbody>
